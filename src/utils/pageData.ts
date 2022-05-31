0e8d11761f3a06190c5d5a078643fbc937c2cbfe
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-05-31 18:32:23 -0400

    Add dynamic description meta tag

--- src/utils/pageData.ts
+++ src/utils/pageData.ts
@@ -2,14 +2,17 @@ export const pages: PageData[] = [
   {
     name: "Home",
     subURL: "",
+    description: "Home page of Github pages personal website.",
   },
   {
     name: "D&D",
     subURL: "dnd",
+    description: "Information and links to my D&D-related things.",
   },
   {
     name: "References",
     subURL: "references",
+    description: "Credits and links for assets on this personal website.",
   },
 ];
 
