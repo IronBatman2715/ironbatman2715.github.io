7e2daab1d37eec7826a17f34fa48ccb8127ea978
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-08-14 19:05:03 -0400

    Refactor if statement to one line

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -4,9 +4,7 @@ import { base } from "$app/paths";
 export function getRelativeURL(subURL = "/"): string {
   let fullRelativeURL = base;
 
-  if (!subURL.startsWith("/")) {
-    fullRelativeURL += "/";
-  }
+  if (!subURL.startsWith("/")) fullRelativeURL += "/";
 
   fullRelativeURL += subURL;
 
