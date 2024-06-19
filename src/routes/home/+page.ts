33862680a25ca86533b952efa96eef0d5829d9d8
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2024-06-19 18:05:16 -0400

    Refactor page redirect imports

--- src/routes/home/+page.ts
+++ src/routes/home/+page.ts
@@ -1,7 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 
+import { getRelativeURL } from "$lib/utils/misc";
 import type { PageLoad } from "./$types";
-import { getRelativeURL } from "@/lib/utils/misc";
 
 export const load: PageLoad = () => {
   redirect(308, getRelativeURL());
