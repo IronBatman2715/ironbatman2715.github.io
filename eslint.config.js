db981e716120ce35af9e6d61fbe41379b736f500
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2024-09-17 15:48:49 -0400

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import svelte from "eslint-plugin-svelte";
 import prettier from "eslint-config-prettier";
 import globals from "globals";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
@@ -26,11 +26,13 @@ export default [
         parser: ts.parser,
       },
     },
-    rules: {
-      "no-undef": "off", // https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-    },
   },
   {
     ignores: ["build/", ".svelte-kit/", "dist/"],
+  },
+  {
+    rules: {
+      "no-undef": "off", // https://typescript-eslint.io/linting/troubleshooting/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+    },
   },
 ];
