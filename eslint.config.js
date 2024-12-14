14418e37d059f4edfa57e5005cb9a476ee779e0d
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2024-12-14 16:01:52 -0500

    Update to svelte@5 and create-svelte -> sv * Bump CI node version to LTS * Allow CI to install with updated lock file if necessary * Override `cookie` until sveltekit bumps * Add missing `importHelpers` in tsconfig.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
-import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import eslint from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
 import svelte from "eslint-plugin-svelte";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import { fileURLToPath } from "node:url";
+import tsEslint from "typescript-eslint";
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
-export default tseslint.config(
+export default tsEslint.config(
+  includeIgnoreFile(gitignorePath),
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsEslint.configs.recommended,
   ...svelte.configs["flat/recommended"],
   prettier,
   ...svelte.configs["flat/prettier"],
@@ -20,14 +24,12 @@ export default tseslint.config(
   },
   {
     files: ["**/*.svelte"],
+
     languageOptions: {
       parserOptions: {
-        parser: tseslint.parser,
+        parser: tsEslint.parser,
       },
     },
-  },
-  {
-    ignores: ["build/", ".svelte-kit/", "dist/"],
   },
   {
     rules: {
